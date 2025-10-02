c01eba7f0fa5a39dd7943bc7a1a129d87886f952
Snehasajeevan2005 <220701281@rajalakshmi.edu.in>
2025-10-02 16:52:52 +0530

    final

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 # Use Existing Resource Group
 # ========================
 data "azurerm_resource_group" "existing" {
-  name = "sneha"
+  name = "sneha-demo"
 }
 
 # ========================
